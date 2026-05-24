8c76256d007ad998ba57f48b992c8053163e0c7c
34j <34j.github@proton.me>
2026-05-24 15:45:05 +0900

    feat: wip

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,24 +1,61 @@
 import { compile, createTrack, runDeclarative, runProcedural, useCompiled, useRef } from 'procedural-to-declarative'
-import { AbsoluteFill, Composition, interpolateColors, spring, useCurrentFrame, useVideoConfig } from 'remotion'
+import { interpolate, AbsoluteFill, Composition, interpolateColors, Series, spring, useCurrentFrame, useVideoConfig } from 'remotion'
 
-const ExampleComparison: React.FC = () => {
+export const ExampleCompOriginal: React.FC = () => {
+  const frame = useCurrentFrame()
+  const { fps } = useVideoConfig()
+
+  const colorChange = interpolate(frame, [0, 60], [0, 1], {
+    extrapolateRight: 'clamp',
+  })
+
+  const spr = spring({
+    fps,
+    frame: frame - 60,
+  })
+  const translateX = interpolate(spr, [0, 1], [0, 300])
+
+  return (
+    <AbsoluteFill
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div
+        style={{
+          width: 200,
+          height: 200,
+          borderRadius: 100,
+          backgroundColor: interpolateColors(
+            colorChange,
+            [0, 1],
+            ['#e6a700', '#e13238'],
+          ),
+          transform: `translateX(${translateX}px)`,
+        }}
+      />
+    </AbsoluteFill>
+  )
+}
+
+const ExampleCompProcedural: React.FC = () => {
   const track = createTrack()
   const color = useRef<string>(track, '#e6a700')
   const x = useRef<number>(track, 0)
-  const frame = useCurrentFrame()
   const { fps } = useVideoConfig()
   function* animation() {
     yield runDeclarative(track, (progress) => {
-      color.current = interpolateColors(progress, [0, 2], ['#e6a700', '#00a7e6'])
+      color.current = interpolateColors(progress, [0, 2], ['#e6a700', '#e13238'])
     }, 2).wait()
     yield runDeclarative(track, (progress) => {
-      x.current = spring({ frame: 1 - progress, fps: 1 })
+      x.current = 300 * spring({ frame: progress * fps, fps })
     }, 1).wait()
   }
   runProcedural(track, animation())
   const compiled = compile(track)
+  const frame = useCurrentFrame()
   useCompiled(track, compiled, frame / fps)
-  console.log('x', x.current)
   return (
     <AbsoluteFill
       style={{
@@ -39,13 +76,26 @@ const ExampleComparison: React.FC = () => {
   )
 }
 
+const ExampleCompCombined: React.FC = () => {
+  return (
+    <Series>
+      <Series.Sequence durationInFrames={90}>
+        <ExampleCompOriginal />
+      </Series.Sequence>
+      <Series.Sequence durationInFrames={90}>
+        <ExampleCompProcedural />
+      </Series.Sequence>
+    </Series>
+  )
+}
+
 export const RemotionRoot: React.FC = () => {
   return (
     <>
       <Composition
         id="OddFrameAudio"
-        component={ExampleComparison}
-        durationInFrames={60}
+        component={ExampleCompCombined}
+        durationInFrames={180}
         fps={30}
         width={640}
         height={360}
