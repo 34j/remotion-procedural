6edec180928e25680c8b692dc5b0afd25df5ab62
34j <34j.github@proton.me>
2026-05-29 21:25:20 +0900

    docs: add more docs

--- src/ExampleProcedural.tsx
+++ src/ExampleProcedural.tsx
@@ -4,25 +4,44 @@ import { AbsoluteFill, interpolateColors, spring, useCurrentFrame, useVideoConfi
 
 export const ExampleProcedural: React.FC = () => {
   const { fps } = useVideoConfig()
+
+  // Memorize the compiled track
   const { track, color, x, compiled } = useMemo(() => {
     const track = createTrack()
+
+    // Refs to hold the current values of parameters that will be animated
     const color = useRef<string>(track, '#e6a700')
     const x = useRef<number>(track, 0)
+
+    // Procedural function that defines the animation sequence
     function* animation() {
+      // First, change the color
       yield runDeclarative(track, (progress) => {
         color.current = interpolateColors(progress, [0, 2], ['#e6a700', '#e13238'])
       }, 2)
       color.current = '#e13238'
+
+      // Then, move the circle horizontally
       yield runDeclarative(track, (progress) => {
         x.current = 300 * spring({ frame: progress * fps, fps })
       }, 1)
     }
+
+    // Top-level call
     runProcedural(track, animation())
+
+    // Compile the track
     const compiled = compile(track)
     return { track, color, x, compiled }
   }, [fps])
+
+  // Use the compiled track at current time
+  // Ref.current is (re)set to the desired value at the current time
   const frame = useCurrentFrame()
   useCompiled(track, compiled, frame / fps)
+
+  // Return the React component with parameters
+  // specified using `Ref.current`
   return (
     <AbsoluteFill
       style={{
